861c99f696c84366909795db5906d73278c4470a
Larene Le Gassick <llegassick@datalust.co>
2020-02-17 11:03:00 +1000

    works for (optional) structured data

--- squiflog/src/lib.rs
+++ squiflog/src/lib.rs
@@ -9,14 +9,14 @@ pub struct Syslog {
     app_name: String,
     proc_id: String,
     message_id: String,
-    structured_data: String,
-    message: String,
+    structured_data: Option<String>,
+    message: Option<String>,
 }
 
 impl Syslog {
     pub fn from_str(s: &str) -> Result<Self, Error> {
-        // split syslog string into elements
-        let mut items = s.splitn(8," ");
+        // split syslog string into elements up to structured data and (message)
+        let mut items = s.splitn(7," ");
 
         // get priority, e.g. "<30>"
         let pri_version = items.next().expect("Invalid syslog message.");
@@ -49,10 +49,43 @@ impl Syslog {
         let app_name = items.next().ok_or("Missing app_name.")?;
         let proc_id = items.next().ok_or("Missing proc_id.")?;
         let message_id = items.next().ok_or("Missing message_id.")?;
-        let structured_data = items.next().ok_or("Missing structured data.")?;
-        let message = items.next().ok_or("Missing message.")?.trim();
 
+        let sd_and_msg = items.next().ok_or("Missing structured data and/or message.")?;
+
+
+        // should be no more after this
         assert!(items.next().is_none());
+
+        // structured_data - check that next string is "-" or "["
+        let mut structured_data: Option<String> = None;
+        let mut message: Option<String> = None;
+
+        let mut structured_data_chars = sd_and_msg.char_indices();
+        while let Some(item) = structured_data_chars.next() {
+            match item {
+                (0,'-') => {
+                    // No structured_data
+                    message = Some(sd_and_msg.get(2..).ok_or("No message.")?.trim().to_string());
+                    break;
+                },
+                (0,'[') => {
+                    // Has structured data
+                    continue;
+                },
+                (0, _) => {
+                    Err("Invalid syslog format.")?
+                },
+                (idx,']') => {
+                    // include the '[' and ']' in structured_data
+                    structured_data = Some(sd_and_msg[..idx+1].to_string());
+                    message = Some(sd_and_msg.get(idx+1..).ok_or("No message.")?.trim().to_string());
+                    break;
+                },
+                _ => {
+                    continue;
+                },
+            }
+        }
 
         Ok(Syslog {
             priority,
@@ -62,8 +95,8 @@ impl Syslog {
             app_name: app_name.to_owned(),
             proc_id: proc_id.to_owned(),
             message_id: message_id.to_owned(),
-            structured_data: structured_data.to_owned(),
-            message: message.to_owned(),
+            structured_data,
+            message,
         })
     }
 }
@@ -89,6 +122,7 @@ mod tests {
 
     #[test]
     fn parse_rfc5424_syslog_message() {
+        // from docker alpine
         let input = "<30>1 2020-02-13T00:51:39.527825Z docker-desktop 8b1089798cf8 1481 8b1089798cf8 - hello world\n";
 
         let expected = Syslog {
@@ -99,8 +133,8 @@ mod tests {
             app_name: "8b1089798cf8".to_owned(),
             proc_id: "1481".to_owned(),
             message_id: "8b1089798cf8".to_owned(),
-            structured_data: "-".to_owned(),
-            message: "hello world".to_owned(),
+            structured_data: None,
+            message: Some("hello world".to_owned()),
         };
 
         let actual = Syslog::from_str(input).expect("Could not parse input for syslog.");
@@ -118,28 +152,8 @@ mod tests {
     }
 
     #[test]
-    fn parse_rfc5424_syslog_message_with_message_containing_hyphen() {
-        let input = "<30>1 2020-02-13T00:51:39.527825Z docker-desktop 8b1089798cf8 1481 8b1089798cf8 - hello world - oh hey there's more\n";
-
-        let expected = Syslog {
-            priority: Priority { facility: 3, severity: 6 },
-            version: "1".to_owned(),
-            timestamp: "2020-02-13T00:51:39.527825Z".to_owned(),
-            hostname: "docker-desktop".to_owned(),
-            app_name: "8b1089798cf8".to_owned(),
-            proc_id: "1481".to_owned(),
-            message_id: "8b1089798cf8".to_owned(),
-            structured_data: "-".to_owned(),
-            message: "hello world - oh hey there's more".to_owned(),
-        };
-
-        let actual = Syslog::from_str(input).expect("Could not parse input for syslog.");
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn parse_rfc5424_syslog_message_from_syslog_unknown_proc_id() {
+    fn parse_rfc5424_syslog_specs_example_1() {
+        // example 1 from https://tools.ietf.org/html/rfc5424
         let input = "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM’su root’ failed for lonvick on /dev/pts/8\n";
 
         let expected = Syslog {
@@ -150,8 +164,52 @@ mod tests {
             app_name: "su".to_owned(),
             proc_id: "-".to_owned(),
             message_id: "ID47".to_owned(),
-            structured_data: "-".to_owned(),
-            message: "BOM’su root’ failed for lonvick on /dev/pts/8".to_owned(),
+            structured_data: None,
+            message: Some("BOM’su root’ failed for lonvick on /dev/pts/8".to_owned()),
+        };
+
+        let actual = Syslog::from_str(input).expect("Could not parse input for syslog.");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_rfc5424_syslog_specs_example_2() {
+        // example 2 from https://tools.ietf.org/html/rfc5424
+        let input = "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts.\n";
+
+        let expected = Syslog {
+            priority: Priority { facility: 20, severity: 5 },
+            version: "1".to_owned(),
+            timestamp: "2003-08-24T05:14:15.000003-07:00".to_owned(),
+            hostname: "192.0.2.1".to_owned(),
+            app_name: "myproc".to_owned(),
+            proc_id: "8710".to_owned(),
+            message_id: "-".to_owned(),
+            structured_data: None,
+            message: Some("%% It's time to make the do-nuts.".to_owned()),
+        };
+
+        let actual = Syslog::from_str(input).expect("Could not parse input for syslog.");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_rfc5424_syslog_specs_example_3() {
+        // example 3 from https://tools.ietf.org/html/rfc5424
+        let input = "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] BOMAn application event log entry...\n";
+
+        let expected = Syslog {
+            priority: Priority { facility: 20, severity: 5 },
+            version: "1".to_owned(),
+            timestamp: "2003-10-11T22:14:15.003Z".to_owned(),
+            hostname: "mymachine.example.com".to_owned(),
+            app_name: "evntslog".to_owned(),
+            proc_id: "-".to_owned(),
+            message_id: "ID47".to_owned(),
+            structured_data: Some("[exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"]".to_owned()),
+            message: Some("BOMAn application event log entry...".to_owned()),
         };
 
         let actual = Syslog::from_str(input).expect("Could not parse input for syslog.");
