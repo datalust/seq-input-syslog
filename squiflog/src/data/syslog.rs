83379fc521d1789e42a90dda6991be0ab789b694
Larene Le Gassick <llegassick@datalust.co>
2020-02-26 08:41:59 +1000

    add severity and facility keywords to log info

--- squiflog/src/data/syslog.rs
+++ squiflog/src/data/syslog.rs
@@ -16,14 +16,44 @@ impl Priority {
 
     pub fn severity(&self) -> &str {
         match self.severity {
-            0 => "Emergency",
-            1 => "Alert",
-            2 => "Critical",
-            3 => "Error",
-            4 => "Warning",
-            5 => "Notice",
-            6 => "Informational",
-            _ => "Debug",
+            0 => "emerg",
+            1 => "alert",
+            2 => "crit",
+            3 => "err",
+            4 => "warning",
+            5 => "notice",
+            6 => "info",
+            _ => "debug",
+        }
+    }
+
+    pub fn facility(&self) -> &str {
+        match self.facility {
+            0 => "kern",
+            1 => "user",
+            2 => "mail",
+            3 => "daemon",
+            4 => "auth",
+            5 => "syslog",
+            6 => "lpr",
+            7 => "news",
+            8 => "uucp",
+            9 => "cron",
+            10 => "authpriv",
+            11 => "ftp",
+            12 => "ntp",
+            13 => "security",
+            14 => "console",
+            15 => "solaris-cron",
+            16 => "local0",
+            17 => "local1",
+            18 => "local2",
+            19 => "local3",
+            20 => "local4",
+            21 => "local5",
+            22 => "local6",
+            23 => "local7",
+            _ => "unknown",
         }
     }
 }
